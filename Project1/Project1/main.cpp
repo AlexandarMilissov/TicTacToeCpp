2f96baab2aefe16c4ddc2be2db2a0c4505c6fe3c
Alex <alex.milissov@gmail.com>
2020-12-26 17:11:59 +0200

    k

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,81 +1,12 @@
 #include <d2d1_1.h>
 #include <stdlib.h>
 #include <Windows.h>
-<<<<<<< HEAD
 #include "Game.h"
 
 using namespace std;
 
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	Game game(hInstance);
-=======
-#include "resource.h"
-#define IDB_BITMAP1                     101
-
-using namespace std;
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    switch (msg)
-    {
-        case WM_CLOSE:
-        {
-            PostQuitMessage(0);
-            break;
-        }
-    }
-    return DefWindowProc(hWnd, msg, wParam, lParam);
 }
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-    const auto pClassName = "Victory screen";
-
-    WNDCLASSEX wc = { 0 };
-    wc.cbSize = sizeof(wc);
-    wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
-    wc.cbClsExtra = 0;
-    wc.cbWndExtra = 0;
-    wc.hInstance = hInstance;
-    wc.hIcon = nullptr;
-    wc.hCursor = nullptr;
-    wc.hbrBackground = CreatePatternBrush(LoadBitmap(hInstance, MAKEINTRESOURCE(IDB_BITMAP1)));
-    wc.lpszMenuName = nullptr;
-    wc.lpszClassName = pClassName;
-    wc.hIconSm = nullptr;
-
-    RegisterClassEx(&wc);
-
-    HWND hWnd = CreateWindowEx( 0, pClassName, "Congratulations",
-        WS_CAPTION | WS_BORDER | WS_SYSMENU,
-        200, 200, 480, 410, 
-        nullptr,nullptr, hInstance, nullptr);
-
-    ShowWindow(hWnd,SW_SHOW);
-
-    MSG msg;
-    
-    BOOL gResult;
-    while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    /*
-    if (gResult == -1)
-    {
-        return -1;
-    }
-    else
-    {
-        msg.wParam;
-    }*/
-
->>>>>>> 9b3dff82334553dc7832809e69a927d2a6c17a05
-}
-
 
